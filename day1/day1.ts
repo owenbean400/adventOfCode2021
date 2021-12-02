b42cd18c8af438b47a3a796285692450a419bfe8
owenbean400 <owenbean400@gmail.com>
2021-12-02 06:01:54 -0500

    Edit function to console log instead of returning

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,17 +1,17 @@
 import fs from 'fs';
 const data = fs.readFileSync('./day1/day1.txt', 'utf8');
 
-function challenge1(): string {
+function day1Challenge1(): void {
     let nums: Array<number> = data.split("\n").map((str: string) => {return parseInt(str)});
     let increaseCnt = 0;
     for (let i = 1; i < nums.length; i++) {
         if (nums[i] - nums[i - 1] > 0)
             increaseCnt++;
     }
-    return increaseCnt.toString();
+    console.log("Day 1, Challenge 1 Answer: " + increaseCnt.toString());
 }
 
-function challenge2() {
+function day1Challenge2(): void {
     let nums: Array<number> = data.split("\n").map((str: string) => {return parseInt(str)});
     let increaseCnt = 0;
     for (let i = 3; i < nums.length; i++) {
@@ -19,7 +19,8 @@ function challenge2() {
         if ((nums[i] + m) - (m + nums[i - 3]) > 0)
             increaseCnt++;
     }
-    return increaseCnt.toString();
+    
+    console.log("Day 1, Challenge 2 Answer: " + increaseCnt.toString());
 }
 
-export { challenge1, challenge2 }
+export { day1Challenge1, day1Challenge2 }
